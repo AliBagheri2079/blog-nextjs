9da0e90d87b806e893ae6f590f2f043084f695ec
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-15 13:05:54 +0330

    :wrench: Chore: update postcss config file with new plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
-    autoprefixer: {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          autoprefixer: {},
+          cssnano: { preset: 'default' },
+        }
+      : {}),
   },
-}
+};
