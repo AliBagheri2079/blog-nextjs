552444ec3fdd6116c24e5d9e96aadf3cee53ec57
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-20 22:42:19 +0330

    :art: Refactor: improve structure with update url constant options

--- utils/constant/urls.js
+++ utils/constant/urls.js
@@ -1,6 +1,7 @@
 const URL = {
-  SPLINE_HOMOCHIRAL:
-    'https://prod.spline.design/RRSzXMpEsuOZUKgE/scene.splinecode',
+  SPLINE: {
+    HOMOCHIRAL: 'https://prod.spline.design/RRSzXMpEsuOZUKgE/scene.splinecode',
+  },
 };
 
 export default URL;
