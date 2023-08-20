cb947746c0db1a34acfb99727190aa84de5cd21d
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-20 22:56:38 +0330

    :art: Refactor: replace blob button style property with shorthand options

--- components/button/Blob/index.styled.jsx
+++ components/button/Blob/index.styled.jsx
@@ -3,28 +3,27 @@ import { Button } from '@/components/ui/button.jsx';
 
 const borderTransform = stitches.keyframes({
   '0%, 100%': {
-    borderRadius: '35% 65% 37% 63% / 35% 72% 28% 65%',
+    br: '35% 65% 37% 63% / 35% 72% 28% 65%',
   },
   '33%': {
-    borderRadius: '72% 28% 48% 48% / 28% 28% 72% 72%',
+    br: '72% 28% 48% 48% / 28% 28% 72% 72%',
   },
   '66%': {
-    borderRadius: '100% 56% 56% 100% / 100% 100% 56% 56%',
+    br: '100% 56% 56% 100% / 100% 100% 56% 56%',
   },
 });
 
 const BlobButton = stitches.styled(Button, {
-  position: 'relative',
+  pos: 'relative',
   width: 'calc(2.5rem + 10vw)',
   height: 'unset',
   aspectRatio: '5 / 3',
 
   '&::before, &::after': {
     content: '',
-    position: 'absolute',
+    pos: 'absolute',
     left: 0,
-    width: '100%',
-    height: '100%',
+    size: '100%',
   },
 
   '&::before': {
@@ -38,18 +37,18 @@ const BlobButton = stitches.styled(Button, {
     zIndex: -1,
     borderStyle: 'solid',
     borderWidth: '1px',
-    borderRadius: '60% 40% 55% 45% / 55% 45% 55% 45%',
+    br: '60% 40% 55% 45% / 55% 45% 55% 45%',
     transform: 'rotate(-20deg)',
     transition: 'transform 0.5s cubic-bezier(0.585, 2.5, 0.645, 0.55)',
   },
 
   '&:hover': {
-    backgroundColor: 'unset',
+    bg: 'unset',
     color: 'unset',
 
     '&::before': {
       animationIterationCount: 0,
-      borderRadius: '60% 40% 50% 50% / 45% 35% 65% 55%',
+      br: '60% 40% 50% 50% / 45% 35% 65% 55%',
     },
 
     '&::after': {
