724d9023420ec609318a6068aca2a205ac9c4fad
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-22 17:25:53 +0330

    :sparkles: Feat: create wave underline text component

--- components/text/index.js
+++ components/text/index.js
@@ -1,1 +1,2 @@
 export { default as RevealText } from './reveal';
+export { default as WaveUnderlineText } from './wave-underline';
---
+++ components/text/wave-underline/index.styled.jsx
@@ -0,0 +1,23 @@
+import { stitches } from '@/lib/config/stitches.config';
+
+const Text = stitches.styled('span', {
+  pos: 'relative',
+  isolation: 'isolate',
+  display: 'inline-block',
+  mx: '0.1rem',
+
+  '& :where(.wave-icon)': {
+    size: '100%',
+    stroke: 'hsl(var(--foreground) / 0.75)',
+    strokeWidth: 1,
+    pointerEvents: 'none',
+  },
+
+  '& > .wave-icon--position': {
+    pos: 'absolute',
+    inset: '40% auto 0 0',
+    zIndex: -1,
+  },
+});
+
+export default Text;
