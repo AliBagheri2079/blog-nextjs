ad1dbfda328514c24c0f1cbda1c8fa1e5cebc634
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-22 22:30:23 +0330

    :zap: Perf: improve performance with add motion show,hide animation

--- layout/header/main-title/index.jsx
+++ layout/header/main-title/index.jsx
@@ -1,14 +1,25 @@
+import { motion } from 'framer-motion';
+
+import variants from '../variants';
 import { FlexCenter } from '@/components/structure';
 import { WaveUnderlineText } from '@/components/text';
+
+const MotionComponent = motion(FlexCenter);
 
 const MainTitle = () => {
   return (
     <div className='col-span-6 row-span-3 row-start-3 md:col-span-4 md:col-start-2 md:row-span-5 md:row-start-2'>
-      <FlexCenter className='h-full'>
+      <MotionComponent
+        initial='hide'
+        animate='show'
+        exit='hide'
+        variants={variants.scale}
+        className='h-full'
+      >
         <h1 className='line-clamp-6 max-w-md text-center font-monument_ultrabold text-5xl capitalize !leading-tight tracking-tight md:max-w-2xl md:text-7xl lg:text-8xl'>
           making <WaveUnderlineText>good</WaveUnderlineText> shit since 2022
         </h1>
-      </FlexCenter>
+      </MotionComponent>
     </div>
   );
 };
