33151ddbae805e31f94d974b7c02c24db5c4470f
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-22 21:29:14 +0330

    :sparkles: Feat: create use typed hook

---
+++ hooks/use-typed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typed from 'typed.js';
+
+const useTyped = (element, options) => {
+  React.useEffect(() => {
+    const typed = new Typed(element?.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  });
+};
+
+export default useTyped;
