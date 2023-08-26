3ea7888b571d088f048382ef7a78a0c7d4045fe0
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-26 20:38:36 +0330

    :lipstick: Style: update home title slides background style

--- layout/home/content/title-slides/index.jsx
+++ layout/home/content/title-slides/index.jsx
@@ -3,7 +3,7 @@ import { ParallaxProgress, ParallaxText } from '@/features/parallax';
 const TitleSlides = () => {
   return (
     <section className='sticky inset-x-0 top-0 z-10'>
-      <div className='bg-primary/90 p-2 font-monument_ultrabold'>
+      <div className='bg-background/90 p-2 font-monument_ultrabold'>
         <ParallaxText baseVelocity={-5}>Just for fun</ParallaxText>
         <ParallaxText baseVelocity={5}>Animate on scroll</ParallaxText>
       </div>
