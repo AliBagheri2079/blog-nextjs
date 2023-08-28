9753a6b33fd6e57cfb0be7402e0e8bff96908fdf
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-28 20:55:41 +0330

    :art: Refactor: improve metadata structure with move to page file

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
 import { Content, Header, Backdrop } from '@/layout/home';
 
+export const metadata = {
+  title: 'Home | Blog',
+  description: 'Story telling about nothing...',
+};
+
 const Home = () => {
   return (
     <>
