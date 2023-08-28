20aef10e4eaf8eddc625f62a5766e8c69294c265
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-28 20:57:57 +0330

    :bug: Bug: fix offcanvas toggle state when navigation

--- layout/public/offcanvas/index.jsx
+++ layout/public/offcanvas/index.jsx
@@ -1,5 +1,6 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 import OffcanvasToggle from './toggle';
 import OffcanvasNavbar from './navbar';
@@ -7,6 +8,17 @@ import { Sheet } from '@/components/ui/sheet';
 
 const Offcanvas = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const currentPath = useRef(null);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (currentPath.current !== pathname) {
+      currentPath.current = pathname;
+      setIsMenuOpen(false);
+    }
+
+    return () => (currentPath.current = null);
+  }, [pathname]);
 
   return (
     <Sheet onOpenChange={status => setIsMenuOpen(status)}>
