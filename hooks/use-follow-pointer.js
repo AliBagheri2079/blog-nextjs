1944af25df999c003be17cdd63b3279afa455285
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-28 09:00:07 +0330

    :art: Refactor: improve useFollowPointer hook structure

--- hooks/use-follow-pointer.js
+++ hooks/use-follow-pointer.js
@@ -2,31 +2,23 @@
 import { useState, useCallback } from 'react';
 
 const useFollowPointer = element => {
-  const [point, setPoint] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const handlePointerMove = useCallback(event => {
-    if (element.current) {
-      const rect = element.current.getBoundingClientRect();
+    const { clientX, clientY } = event;
+    const { width, height, left, top } =
+      element?.current.getBoundingClientRect();
 
-      const offsetX = event.clientX - rect.left;
-      const offsetY = event.clientY - rect.top;
-
-      const elmWidth = rect.width;
-      const elmHeight = rect.height;
-
-      const x = offsetX - elmWidth / 3;
-      const y = offsetY - elmHeight / 3;
-      setPoint({ x, y });
-    }
+    const x = clientX - (left + width / 3);
+    const y = clientY - (top + height / 3);
+    setPosition({ x, y });
   }, []);
 
   const handlePointerOut = useCallback(() => {
-    if (element.current) {
-      setPoint({ x: 0, y: 0 });
-    }
+    setPosition({ x: 0, y: 0 });
   }, []);
 
-  return [point, handlePointerMove, handlePointerOut];
+  return [position, handlePointerMove, handlePointerOut];
 };
 
 export default useFollowPointer;
