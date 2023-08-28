90d9d41c196a2f14abe5c3429fdec69f44d86271
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-08-28 18:42:05 +0330

    :sparkles: Feat: add two spline items url

--- utils/constant/urls.js
+++ utils/constant/urls.js
@@ -1,6 +1,10 @@
 const URL = {
   SPLINE: {
-    HOMOCHIRAL: 'https://prod.spline.design/RRSzXMpEsuOZUKgE/scene.splinecode',
+    HOMO_CHIRAL: 'https://prod.spline.design/RRSzXMpEsuOZUKgE/scene.splinecode',
+    RETRO_GLASS_MATERIAL:
+      'https://prod.spline.design/uTHtfoZuqfB86hwz/scene.splinecode',
+    HELLO_PATH_GROWTH:
+      'https://prod.spline.design/Y989KfJhUodO-ixY/scene.splinecode',
   },
 };
 
